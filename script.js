af96e08df65caa63a3e1b6b875440de66ba47894
Miguel Moura <miguel.mourasp@gmail.com>
2024-04-11 18:29:51 -0300

    Reformando o estilo do CRUD, excluindo o server e armazenando infos no script.js

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
-document.getElementById('add-product').addEventListener('click', () => {
-    const data = {
-        id: '1',
-        name: 'Celular',
-        price: 20
-    };
+document.addEventListener('DOMContentLoaded', () => {
+    const productList = document.getElementById('product-list');
+    const productForm = document.getElementById('product-form');
 
-    fetch('http://localhost:/products', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (response.ok) {
-            console.log('Produto adicionado com sucesso!');
-        } else {
-            console.error('Erro ao adicionar produto');
+    
+    function displayProducts() {
+        productList.innerHTML = '';
+        const products = JSON.parse(localStorage.getItem('products')) || [];
+
+        products.forEach((product, index) => {
+            const li = document.createElement('li');
+            li.textContent = `${product.name} - R$ ${product.price}   `;
+
+            
+            const deleteImage = document.createElement('img');
+            deleteImage.src = './assets/image/Lixeira.png'; 
+            deleteImage.alt = 'Excluir';
+            deleteImage.classList.add('delete-image'); 
+            deleteImage.addEventListener('click', () => {
+                
+                products.splice(index, 1);
+                localStorage.setItem('products', JSON.stringify(products));
+                displayProducts(); 
+            });
+
+            li.appendChild(deleteImage);
+            productList.appendChild(li);
+        });
+    }
+
+    
+    productForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const price = document.getElementById('price').value;
+
+        if (!name || !price) {
+            alert('Por favor, preencha todos os campos.');
+            return;
         }
-    })
-    .catch(error => {
-        console.error('Erro ao adicionar produto:', error);
+
+        const product = { name, price };
+        const products = JSON.parse(localStorage.getItem('products')) || [];
+        products.push(product);
+        localStorage.setItem('products', JSON.stringify(products));
+
+        productForm.reset();
+        displayProducts();
     });
+
+    
+    displayProducts();
 });
